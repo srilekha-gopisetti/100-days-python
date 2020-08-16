64ec059bc644d9c9e7feb540167f017cd0b0d4c4
srilekha-gopisetti <69568315+srilekha-gopisetti@users.noreply.github.com>
2020-08-16 11:21:33 +0530

    Add files via upload

---
+++ prime number using for loop.py
@@ -0,0 +1,25 @@
+##n=int(input("enter any number:"))
+##count==0
+##for i in range(1,n+1):
+##    if n%i==0:
+##        count+=1
+##if count==2:
+##    print(n,"prime number")
+##else:
+##    print(n,"not a prime number")
+##n=int(input("enter n value"))
+##count=0
+##for i in range(2 ,n):#5%2,5%3,#5%4
+##    if n%i==0:
+##      count+=1
+#if count>0:
+#    print(n,"not prime number")
+#else:
+#    print(n,"prime number")
+n=int(input("enter a number"))
+for i in range(2,n):
+    if n%i==0:
+        print(n,'not a primenumber')
+        break
+else:
+    print(n,'primenumber')
