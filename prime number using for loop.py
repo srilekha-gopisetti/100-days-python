84fd434c59f4a409bc1059678682657d5d92b69b
srilekha-gopisetti <69568315+srilekha-gopisetti@users.noreply.github.com>
2020-08-16 11:23:49 +0530

    Update prime number using for loop.py

--- prime number using for loop.py
+++ prime number using for loop.py
@@ -1,3 +1,4 @@
+      code 1
 ##n=int(input("enter any number:"))
 ##count==0
 ##for i in range(1,n+1):
@@ -7,6 +8,7 @@
 ##    print(n,"prime number")
 ##else:
 ##    print(n,"not a prime number")
+      code 2
 ##n=int(input("enter n value"))
 ##count=0
 ##for i in range(2 ,n):#5%2,5%3,#5%4
@@ -16,6 +18,7 @@
 #    print(n,"not prime number")
 #else:
 #    print(n,"prime number")
+     code 3
 n=int(input("enter a number"))
 for i in range(2,n):
     if n%i==0:
