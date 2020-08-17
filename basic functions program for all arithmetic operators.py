ce64d2072e41b2a6baa83e8aebfbf196461f0835
srilekha-gopisetti <69568315+srilekha-gopisetti@users.noreply.github.com>
2020-08-17 11:47:52 +0530

    Add files via upload

---
+++ basic functions program for all arithmetic operators.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return(a+b)
+def sub(a,b):
+    return(a-b)
+def mul(a,b):
+    return(a*b)
+def division(a,b):
+    return(a/b)
+a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+print(add(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(division(a,b))
